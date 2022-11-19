c8f7f2576ac4396f71f92c8cc8e680e2d944b613
yoshimochi <olive.oil6319@gmail.com>
2022-11-19 23:27:15 +0900

    素数の取り扱い

---
+++ isPrime/is_prime1.py
@@ -0,0 +1,15 @@
+# 素数を探索
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+for i in range(200):
+    if is_prime(i):
+        print(i, end=' ')
