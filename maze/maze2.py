4e659d28d6cbdaf6f60c2ebcff30203ccb801a17
yoshimochi <olive.oil6319@gmail.com>
2022-11-23 12:38:24 +0900

    迷路_幅優先探索_maze.py 迷路_深さ優先探索_maze2.py

--- maze/maze2.py
+++ maze/maze2.py
@@ -1,10 +1,6 @@
 """
-幅優先探索
-
-壁を９、通路を０、ゴールを１で表現
+深さ優先探索
 """
-START_LOG = '処理開始'
-END_LOG = '処理終了'
 maze = [
     [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
     [9, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 9],
@@ -20,31 +16,27 @@ maze = [
     [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
 ]
 
-# スタート位置(x座標、y座標、移動回数)をセット
-pos = [[1, 1, 0]]
-
-print(START_LOG)
-
-while len(pos) > 0:
-    x, y, depth = pos.pop(0) # リストから探索する位置を取得
-
+def search(x, y, depth):
     # ゴールに着くと終了
     if maze[x][y] == 1:
         print(depth)
-        print(END_LOG)
-        break
+        exit()
 
-    #　探索済みとしてセット
-    maze[x][y] == 2
+    # 探索済みとしてセット
+    maze[x][y] = 2
 
     # 上下左右を探索
     if maze[x - 1][y] < 2:
-        pos.append([x - 1, y, depth + 1])
+        search(x - 1, y, depth + 1) # 移動回数を増やして左を探索
     if maze[x + 1][y] < 2:
-        pos.append([x + 1, y, depth + 1])
+        search(x + 1, y, depth + 1)  # 移動回数を増やして右を探索
     if maze[x][y - 1] < 2:
-        pos.append([x, y - 1, depth + 1])
+        search(x, y - 1, depth + 1)  # 移動回数を増やして上を探索
     if maze[x][y + 1] < 2:
-        pos.append([x, y + 1, depth + 1])
+        search(x, y + 1, depth + 1)  # 移動回数を増やして下を探索
 
-    print('{}, {}, {}'.format('現在の移動回数は', depth, 'です'))
+    # 探索前に戻す
+    maze[x][y] = 0
+
+# スタート位置を探索を開始
+search(1, 1, 0)
