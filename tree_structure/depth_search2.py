5d7d1ad36fdb7b3a99936e810fd91325938898ec
yoshimochi <olive.oil6319@gmail.com>
2022-12-04 22:25:50 +0900

    木構造についてまとめ

--- tree_structure/depth_search2.py
+++ tree_structure/depth_search2.py
@@ -2,6 +2,7 @@
 tree = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [], [], [], [], [], [], [], []]
 
 def search(pos):
+    print(tree[pos])
     for i in tree[pos]:
         search(i)
     print(pos, end=' ')
